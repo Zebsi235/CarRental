25e49e844f491087e88ae0e8afad35e7581aaf49
Sebastian Mück <S1910306035@students.fh-Hagenberg.at>
2020-12-19 16:31:55 +0100

    Files

---
+++ AirConditioner.h
@@ -0,0 +1,13 @@
+////////////////////////////////
+// Workfile : main.cpp
+// Date : 19.12.2020
+// Name : 
+// Description : Testing Driver
+////////////////////////////////
+
+
+#ifndef CARRENTAL_H
+#define CARRENTAL_H
+
+
+#endif
